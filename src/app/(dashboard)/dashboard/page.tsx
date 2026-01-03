416955af34e6a5821d94b1acad5af7b105bce5a4
Davie Karanja <80883320+daviekaranja@users.noreply.github.com>
2026-01-03 15:22:16 +0300

    adds google oath to the dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,15 @@
+"use client";
 import React from "react";
+import { useAuthenticatedUser } from "@/lib/hooks/useUser";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const user = useAuthenticatedUser();
+  return <div>Dashboard Welcome, {user?.user?.name}!
+    {/* big and bold welcome message here */}
+    
+    {/* some analytics cards here, use dummy values for now */}
+  
+  </div>;
 };
 
 export default Dashboard;
--- src/proxy.ts
+++ src/proxy.ts
@@ -4,17 +4,18 @@ import { getToken } from "next-auth/jwt";
 const authUrl = "/";
 
 export async function proxy(req: NextRequest) {
-  console.log("PROXY CALLED:", req.url);
-
+  console.log("Proxy middleware called for:", req.url);
   const token = await getToken({
     req,
     secret: process.env.AUTH_SECRET, // make sure this matches your env
   });
 
-  const accessToken = token?.accessToken as string | undefined;
+
+  const accessToken = token?.accessToken as string;
 
   // No access token → redirect
   if (!accessToken) {
+    console.error("No access token found");
     return NextResponse.redirect(new URL(authUrl, req.url));
   }
 
@@ -29,7 +30,15 @@ export async function proxy(req: NextRequest) {
       return NextResponse.redirect(new URL(authUrl, req.url));
     }
 
+    const data = await res.json();
+
+    if (!data.email) {
+      console.error("No user found");
+      return NextResponse.redirect(new URL(authUrl, req.url));
+    }
+
     // Token valid → allow request
+    console.log("User authenticated:", data.email);
     return NextResponse.next();
   } catch {
     // Backend unreachable → fail closed
@@ -43,4 +52,3 @@ export async function proxy(req: NextRequest) {
 export const config = {
   matcher: ["/dashboard/:path*"],
 };
- 
